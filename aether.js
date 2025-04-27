355076488f60058cbff62be0ea6838cd2813ea25
Emonora <emonora@unix.dog>
2025-04-27 15:16:15 -0500

    fix #2

--- aether.js
+++ aether.js
@@ -1,32 +1,18 @@
-// src/prism-aether.js
-/**
- * Prism language definition for Aether (your custom DSL)
- */
-export default function (Prism) {
+(function (Prism) {
   Prism.languages.aether = {
-    // 1. Comments
     comment: /\/\/.*/,
-
-    // 2. Keywords
     keyword:
-      /\b(?:for|dec|declare|as|import|export|type|function|repeat|if|else|config|with|global|var)\b/,
-
-    // 3. Selectors: @class and #id
+      /\b(?:for|dec|as|import|export|function|type|repeat|if|else|with|global|config)\b/,
     selector: {
-      pattern: /[@#][a-zA-Z_]\w*/,
+      pattern: /[@#][\w-]+/,
       alias: "symbol",
     },
-
-    // 4. Strings (single & double quotes)
+    "attr-name": /\b[a-zA-Z_]\w*(?=\s*(?:[:=]|with|as)\b)/,
+    number: /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
+    operator: /[{}();,]|=>|\.\.\.|[<>]=?|[=+*/%-]/,
     string: {
       pattern: /(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,
       greedy: true,
     },
-
-    // 5. Numbers (hex, decimal)
-    number: /\b0x[\da-fA-F]+\b|\b\d+(?:\.\d+)?\b/,
-
-    // 6. Operators & punctuation
-    operator: /[{}();:,]/,
   };
-}
+})(Prism);
